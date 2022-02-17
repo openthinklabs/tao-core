4562d8d1cdb05c492b6a4c4a9b61dac4ec179519
Wildan Maulana <wildan.m@gmail.com>
2022-02-17 18:10:37 +0700

    fix typo default config

---
+++ config/header/js.conf.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Default config header created during install
+ */
+
+return new oat\oatbox\config\ConfigurationService(array(
+    'config' => array(
+        'timeout' => 300,
+        'crossorigin' => false
+    )
+));
--- config/header/theming.conf.php
+++ config/header/theming.conf.php
@@ -4,13 +4,6 @@
  * be used by the php backend
  */
 
-<?php
-
-/**
- * The theming service is used to determine which theme should
- * be used by the php backend
- */
-
 return new oat\tao\model\theme\ThemeService(array(
     'available' => array(
         'default' => new oat\tao\model\theme\DefaultTheme(),
